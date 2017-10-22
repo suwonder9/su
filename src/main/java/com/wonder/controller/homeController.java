b6c6d46032b7acfb3821c84260be56bf391f2f40
Administrator <suwonder9@163.com>
2017-10-22 10:37:52 +0800

    添加thymeleaf

--- src/main/java/com/wonder/controller/homeController.java
+++ src/main/java/com/wonder/controller/homeController.java
@@ -1,16 +1,16 @@
 package com.wonder.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Administrator on 2017/10/21 0021.
  */
-@RestController
+@Controller
 public class homeController {
 
     @RequestMapping("/")
     public String home(){
-        return "welcome home";
+        return "/home";
     }
 }
