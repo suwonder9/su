8ceb729d907d41d7368e51c8456525043be6c995
sujinxian <13423621701xian!>
2019-04-09 17:44:51 +0800

    完善功能

--- src/main/java/com/wonder/entity/individualTicketVo.java
+++ src/main/java/com/wonder/entity/individualTicketVo.java
@@ -2,14 +2,34 @@ package com.wonder.entity;
 
 import lombok.Data;
 
-import java.math.BigDecimal;
-
 @Data
-public class individualTicketVo {
+public class IndividualTicketVo {
+    private String orderId;
+    private String orderDate;
+    private String productName;
+    private String printTicketName;
+    private String proxyName;
+    private String parkName;
+    private Double quantity;
+    private Double tranAmount;
+    private Double settlePrice;
+    private String contactMobile;
+    private String referenceNum;
+    private String startDate;
+    private String endDate;
+    private String showStartTime;
+    private String itemStatus;
+    private String rebateType;
+    private String pickupTime;
+    private String pickupType;
+    private String region;
+    private String otaOrderId;
     private String accountId;
-
-    private BigDecimal orderId;
-
+    private String accountType;
+    private String userName;
+    private String orderSource;
     private String accountName;
+
+
 
 }
